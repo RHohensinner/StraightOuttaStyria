22bbded4294b06a520f1453f79d38b78fa415d07
Richard Hohensinner <richard.hohensinner@student.tugraz.at>
2019-12-09 20:18:16 +0100

    further work on demo stage

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Internal;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterScript : MonoBehaviour
+{
+ 
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public bool isGrounded;
+    public float jumpForce;
+    public float speed;
+    Rigidbody2D rb;
+    public float distToGround;
+
+    void Start ()
+    {
+        rb = GetComponent <Rigidbody2D> ();
+        distToGround = rb.position.y;
+    }
+ 
+    //bool IsPlayerGrounded()
+    //{
+    //    return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1));
+    //}
+ 
+    void Update () {
+        if (Input.GetButtonDown ("Jump") && isGrounded) {
+            rb.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+        float y = rb.position.y;
+
+        if(y < -3)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+ 
+    void FixedUpdate ()
+    {
+        //if(isPlayerGrounded())
+        //{
+        //    isGrounded = true;
+        //}
+
+        //isGrounded = Physics2D.OverlapPoint(groundCheck.position, whatIsGround);
+        //Debug.Log("HELLO");
+        float x = Input.GetAxis ("Horizontal");
+        Vector3 move = new Vector3 (x * speed, rb.velocity.y, 0f);
+        rb.velocity = move;
+    }
+
+    //make sure u replace "floor" with your gameobject name.on which player is standing
+    void OnCollisionEnter2D(Collision2D theCollision)
+    {
+        //Debug.Log(theCollision.gameObject.name);
+        LayerMask ground = LayerMask.GetMask("Ground");
+        if (theCollision.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+
+}
