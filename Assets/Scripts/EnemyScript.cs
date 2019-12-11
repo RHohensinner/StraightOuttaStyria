5372c92238f077cd4c876adc22b3dce9d11aee67
Inti Mendoza <intigabi13@gmail.com>
2019-12-12 00:47:44 +0100

    added vienna enemy + some scripts (also for Player)

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Internal;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyScript : MonoBehaviour
+{
+	//private bool hasSpawn; // for when level too bicc
+	private WeaponScript[] weapons;
+	private Collider2D colliderComponent;
+	private SpriteRenderer rendererComponent;
+
+	/* For when level too bicc
+	void Awake() 
+	{
+		
+	}
+	*/
+
+	void Start()
+	{
+		weapons = GetComponentsInChildren<WeaponScript>();
+		colliderComponent = GetComponent<Collider2D>();
+		rendererComponent = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		foreach (WeaponScript weapon in weapons)
+		{
+			if(weapon != null && weapon.can_attack)
+			{
+				weapon.Attack(true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HealthScript : MonoBehaviour
+{
+	
+	public int hp = 1; // enemy base hp
+
+	public bool isEnemy = true;
+
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+
+		if (hp <= 0)
+		{
+			// entity shot gets yeeted
+			// SpecialEffectsHelper.Instance.Explosion(transform.position);
+			// SoundEffectsHelper.Instance.MakeExplosionSound();
+
+			// Yeet!
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+		if (shot != null)
+		{
+			// Avoid friendly fire
+			if (shot.isEnemyShot != isEnemy)
+			{
+				Damage(shot.damage);
+				// Destroy the yeeting tool
+				Destroy(shot.gameObject); // shot gets yeeted
+			}
+		}
+	}
+}
