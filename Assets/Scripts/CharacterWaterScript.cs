31977b99b0027565d6554a485bd693cbbf315522
Richard Hohensinner <richard.hohensinner@student.tugraz.at>
2020-03-04 23:10:59 +0100

    water stage

---
+++ Assets/Scripts/CharacterWaterScript.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Internal;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterWaterScript : MonoBehaviour
+{
+ 
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public bool isGrounded;
+    public bool firePickedUp;
+    public bool fireActive;
+    public bool potionPickedUp;
+    public bool potionActive;
+    public bool shieldPickedUp;
+    public bool shieldActive;
+    public float jumpForce;
+    public float speed;
+    Rigidbody2D rb;
+    public float distToGround;
+
+    public int shieldDamageCounter;
+    public int shieldExpirationCounter;
+    public int fireCounter;
+
+    private AudioSource audio;
+    public AudioClip audio_jump;
+
+    void Start ()
+    {
+        rb = GetComponent <Rigidbody2D> ();
+        distToGround = rb.position.y;
+
+        InvokeRepeating("UpdateShieldExpiration", 1f, 1f);
+    }
+ 
+    //bool IsPlayerGrounded()
+    //{
+    //    return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1));
+    //}
+ 
+    void Update () {
+        if (Input.GetButtonDown ("Jump") && isGrounded) {
+            rb.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(audio_jump);
+        }
+
+        float y = rb.position.y;
+
+        if(y < -3)
+        {
+            //Destroy(gameObject);
+        }
+
+        if(Input.GetButtonDown("Potion") && potionPickedUp)
+        {
+            Debug.Log("Potion used!");
+            HealthScript health = GetComponent<HealthScript>();
+            if(health != null)
+            {
+                health.hp = health.maxHealth;
+                health.healthBar.SetSize(1f);
+            }
+            
+            potionPickedUp = false;
+        }
+
+        if(Input.GetButtonDown("Shield") && shieldPickedUp)
+        {
+            Debug.Log("Shield used!");
+            shieldActive = true;
+            shieldDamageCounter = 3;
+            shieldExpirationCounter = 10;
+            shieldPickedUp = false;
+        }
+
+        if(shieldActive && shieldExpirationCounter == 0)
+        {
+            Debug.Log("Shield expired!");
+            shieldActive = false;
+            shieldDamageCounter = 0;
+        }
+    }
+ 
+    void FixedUpdate ()
+    {
+        //isGrounded = Physics2D.OverlapPoint(groundCheck.position, whatIsGround);
+        if(Input.GetButtonDown("Left"))
+        {
+            //Debug.Log("Turning left!");
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if(Input.GetButtonDown("Right"))
+        {
+            //Debug.Log("Turning right!");
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        float x = Input.GetAxis ("Horizontal");
+        Vector3 move = new Vector3 (x * speed, rb.velocity.y, 0f);
+        rb.velocity = move;
+    }
+
+    //make sure u replace "floor" with your gameobject name.on which player is standing
+    void OnCollisionEnter2D(Collision2D theCollision)
+    {
+        //Debug.Log(theCollision.gameObject.name);
+        LayerMask ground = LayerMask.GetMask("Ground");
+        if (theCollision.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+
+        if (theCollision.gameObject.layer == 11)
+        {
+            // fire item
+            Debug.Log("Fire picked up!");
+            firePickedUp = true;
+            //fireActive = true;
+            fireCounter = 5;
+        }
+        if (theCollision.gameObject.layer == 12)
+        {
+            // potion item
+            Debug.Log("Potion picked up!");
+            potionPickedUp = true;
+            //potionActive = true;
+        }
+        if (theCollision.gameObject.layer == 13)
+        {
+            // shield item
+            Debug.Log("Shield picked up!");
+            shieldPickedUp = true;
+            //shieldActive = true;
+        }
+        if (theCollision.gameObject.layer == 15)
+        {
+        	Debug.Log("inside Water!");
+        }
+    }
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Debug.Log("inside Water!");
+		//isGrounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) 
+	{
+		//Debug.Log("outside Water!");
+	}
+
+	void OnTriggerStay2D(Collider2D other) 
+	{
+		//Debug.Log("while Water!");
+		isGrounded = true;
+	}
+
+
+    void UpdateShieldExpiration()
+    {
+        if (shieldActive)
+            shieldExpirationCounter--;
+    }
+}
---
+++ Assets/Scripts/WeaponWaterScript.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponWaterScript : MonoBehaviour
+{
+    public Transform firePoint;
+	public Transform shot_shot;
+    public CharacterWaterScript player;
+    public LineRenderer lineRenderer;
+
+	public float shoot_rate = 0.01f;
+	public float shoot_cd;
+    public int laserDmg = 1;
+
+    private AudioSource audio_laser;
+
+	void Start()
+	{
+		shoot_cd = 0f;
+	}
+
+	public bool can_attack
+	{
+		get
+		{
+			return shoot_cd <= 0 ? true : false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (shoot_cd > 0)
+		{
+			shoot_cd -= Time.deltaTime;
+		}
+		if (player != null)
+		{
+			if (Input.GetButtonDown("Fire") && player.firePickedUp)
+			{
+				if (can_attack)
+				{
+					shoot_cd = shoot_rate;
+					StartCoroutine(Shoot());
+					player.fireCounter--;
+
+					if (player.fireCounter == 0)
+					{
+						player.firePickedUp = false;
+					}
+				}
+			}
+		}
+    }
+
+	public void Attack(bool isEnemy)
+	{
+		if(can_attack)
+		{
+			shoot_cd = shoot_rate;
+			var shotTransform = Instantiate(shot_shot) as Transform;
+			shotTransform.position = transform.position;
+
+			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+			MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+			if (shot != null && move != null)
+			{
+				shot.isEnemyShot = isEnemy;
+				//Debug.Log("direction" + move.direction.ToString());
+			}
+		}
+	}
+
+    IEnumerator Shoot()
+    {
+        audio_laser = GetComponent<AudioSource>();
+        audio_laser.Play();
+        RaycastHit2D hitInfo =  Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if(hitInfo)
+        {
+            Debug.Log(hitInfo.transform.name);
+            EnemyScript enemy = hitInfo.transform.GetComponent<EnemyScript>();
+            ShotScript shot = hitInfo.transform.GetComponent<ShotScript>();
+            if (enemy != null)
+            {
+                
+                HealthScript health = hitInfo.transform.GetComponent<HealthScript>();
+                if(health != null)
+                {
+                    Debug.Log("DIE!!!!");
+                    health.Damage(laserDmg);
+                }
+            }
+            else if(shot != null)
+            {
+                Destroy(shot.gameObject);
+            }
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            Debug.Log("No Hit!");
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        lineRenderer.enabled = false;
+    }
+}
