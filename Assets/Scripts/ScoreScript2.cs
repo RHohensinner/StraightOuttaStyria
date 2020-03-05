c3a48dbbb12da530313dd4859db2bcc3cd7ae1b8
Inti Mendoza <intigabi13@gmail.com>
2020-03-05 19:34:18 +0100

    added score scripts

---
+++ Assets/Scripts/ScoreScript2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript2 : MonoBehaviour
+{
+	public float scoreValue = 120f;
+	Text scoreText;
+	// Start is called before the first frame update
+	void Start()
+    {
+		scoreText = GetComponent<Text>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		scoreValue -= Time.deltaTime;
+		scoreText.text = "Score: " + ((int)scoreValue).ToString();
+    }
+}
